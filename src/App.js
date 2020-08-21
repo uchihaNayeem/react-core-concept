b21d279371ac5b9829b8e7b46d27242273c21b34
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-21 11:57:03 +0600

    this is the FIRST

--- src/App.js
+++ src/App.js
@@ -8,16 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I've started to learn REACT on 21st August, 2020.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        
       </header>
     </div>
   );
